a85886cdce6bd6a2b9c53cd8b6d2b12694ca4148
Maureen Barahona <maureenbarahona@gmail.com>
2021-01-16 11:31:17 -0600

    Define book

---
+++ personal/src/main/java/hn/mau/personal/Libro/fig3_1/LibroCalificaciones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.mau.personal.Libro.fig3_1;
+
+/**
+ *
+ * @author maureen
+ */
+public class LibroCalificaciones {
+
+    /**
+     */
+    public void mostrarMensaje() {
+        System.out.println("Bienvenido al Libro de calificaciones");
+    }
+
+}
